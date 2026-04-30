337f04f94d41d019492b626e70522fc5981245fc
Phate <phanhuyduc0994@gmail.com>
2026-04-30 15:11:26 +0700

    Modified the Carousel

--- frontend/web-app/components/shared/main-carousel.tsx
+++ frontend/web-app/components/shared/main-carousel.tsx
@@ -37,10 +37,9 @@ export default function MainCarousel({ data }: Props) {
                   <ImageLoader
                     src={banner.url}
                     alt={banner.title}
-                    height={0}
-                    width={0}
-                    sizes="100vw"
-                    className="h-auto w-full"
+                    height={400}
+                    width={1200}
+                    className="h-[400px] w-full object-cover object-center"
                   />
                   <div className="absolute inset-0 flex items-end justify-center">
                     <h2 className="bg-gray-900 opacity-50 text-2xl font-bold px-2 text-white">
